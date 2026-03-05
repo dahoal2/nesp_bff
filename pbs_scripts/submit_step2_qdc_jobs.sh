f52432ef2ecb2052eb6343c5c359c3990d9a2bf5
David Hoffmann <david.hoffmann@bom.gov.au>
2026-03-05 16:10:50 +1100

    qdc rsds + get rsdsdir from fraction

--- pbs_scripts/submit_step2_qdc_jobs.sh
+++ pbs_scripts/submit_step2_qdc_jobs.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 PBS="/home/565/dh4185/mn51-dh4185/repos_collab/nesp_bff/pbs_scripts/pbs_qdc_step2"
 
 # Edit if needed — ideally match utils.locations keys
-#LOCS=(Darwin)
+#LOCS=(Melbourne)
 LOCS=(
   Melbourne Darwin Cairns Brisbane Longreach Mildura Adelaide Perth Sydney Canberra Hobart
 )
